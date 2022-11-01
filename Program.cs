df3b6247fa730741fe5cdf2bebd2684031216090
IlyaLitvak <108228995+IlyaLitvak@users.noreply.github.com>
2022-11-01 15:36:54 +0200

    Create Program.cs
    First version

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿String[] A= {"hello","2","world",";-)"};
+//hhhh
+//String[] A= {"1234","1567","-2","computer science"};
+//String[] A= {"Russia","Denmark","Kazan"};
+List<String> B=new List<String>();
+for(int i=0;i<A.Length;i++){
+    if (A[i].Length <=3){
+        B.Add(A[i]);
+    }
+}
+
+for (int i = 0; i<B.Count; i++){
+    Console.WriteLine(B[i]);
+}
+
