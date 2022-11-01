e008f45cce6cb4be4442a3e13f9a9b62665f6926
IlyaLitvak <108228995+IlyaLitvak@users.noreply.github.com>
2022-11-01 15:38:53 +0200

    Update Program.cs
    Test2

--- Program.cs
+++ Program.cs
@@ -1,7 +1,6 @@
-﻿String[] A= {"hello","2","world",";-)"};
-//hhhh
+﻿//String[] A= {"hello","2","world",";-)"};
 //String[] A= {"1234","1567","-2","computer science"};
-//String[] A= {"Russia","Denmark","Kazan"};
+String[] A= {"Russia","Denmark","Kazan"};
 List<String> B=new List<String>();
 for(int i=0;i<A.Length;i++){
     if (A[i].Length <=3){
